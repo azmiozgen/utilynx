67929bcd57eac530a59aaaf5becab516bc96bb0f
Azmi C. Ozgen <azmi.ozgen@rempeople.com>
2024-08-20 00:45:16 +0300

    Add bunch of functions about dfs-bfs

--- python.py
+++ python.py
@@ -1,4 +1,4 @@
-from collections import Counter
+from collections import Counter, deque
 import json
 import re
 
@@ -17,6 +17,26 @@ class TreeNode:
         self.val = val
         self.left = left
         self.right = right
+
+
+def bfs_by_queue(root):
+    """
+    Breadth-first search using a queue
+    It's a concept function. Arrange below values (NOT_FOUND, FOUND, OK) to make it work.
+    Args:
+        root: TreeNode
+    """
+    NOT_FOUND = None
+    FOUND = lambda node: node
+    OK = lambda node: False
+    queue = deque([root]) # at least one element in the queue to kick start bfs
+    while len(queue) > 0: # as long as there is an element in the queue
+        node = queue.popleft() # dequeue
+        for child in node.children: # enqueue children
+            if OK(child): # early return if problem condition met
+                return FOUND(child)
+            queue.append(child)
+    return NOT_FOUND
 
 
 def bubble_sort(arr):
@@ -60,6 +80,28 @@ def climb_stairs(n):
     for _ in range(2, n+1):
         prev, curr = curr, prev + curr
     return curr
+
+
+def dfs(root, target):
+    """
+    Depth-first search.
+    Uses pre-order traversal (root, left, right).
+    """
+    if root is None:
+        return None
+    if root.val == target:
+        return root
+    # return non-null return value from the recursive calls
+    left = dfs(root.left, target)
+    if left is not None:
+        return left
+
+    # at this point, we know left is null, and right could be null or non-null
+    # we return right child's recursive call result directly because
+    # - if it's non-null we should return it
+    # - if it's null, then both left and right are null, we want to return null
+    return dfs(root.right, target)
+    # the code can be shortened to: return dfs(root.left, target) or dfs(root.right, target)
 
 
 def find_power_set(s):
@@ -247,6 +289,36 @@ def has_cycle(head):
         if fast == slow:
             return True
     return False
+
+
+def in_order_traversal(root):
+    """
+    In-order traversal of a binary tree.
+    Recursively add left, root, and right nodes.
+    """
+    if root is None:
+        return []
+    return in_order_traversal(root.left) + [root.val] + in_order_traversal(root.right)
+
+
+def is_symmetric_tree(root) -> bool:
+    """
+    Check if a binary tree is symmetric using BFS and a queue.
+    """
+    def is_queue_symmetric(q):
+        l = [node.val if node is not None else None for node in q]
+        return l == l[::-1]
+    queue = deque([root])
+    n_max = 2 # Number of nodes in the next level
+    while len(queue) > 0 :
+        node = queue.popleft()
+        queue.extend([node.left, node.right])
+        if len(queue) == n_max:
+            if not is_queue_symmetric(queue):
+                return False
+            queue = deque(filter(lambda x: x is not None, queue)) # Remove None nodes
+            n_max = len(queue) * 2
+    return True
 
 
 def is_valid_parantheses(s):
@@ -441,6 +513,25 @@ if __name__ == '__main__':
     head.next.next = ListNode(0)
     head.next.next.next = head.next
     print(has_cycle(head))
+
+    ## Test in_order_traversal
+    print()
+    print('Test in_order_traversal with a binary tree with [3, 9, 20, null, null, 15, 7]')
+    root = TreeNode(3,
+                    TreeNode(9),
+                    TreeNode(20,
+                             TreeNode(15), TreeNode(7)))
+    print(in_order_traversal(root))
+
+    ## Test is_symmetric_tree
+    print()
+    print('Test is_symmetric_tree with a symmetric binary tree with [1, 2, 2, 3, 4, 4, 3]')
+    root = TreeNode(1,
+                    TreeNode(2,
+                             TreeNode(4), TreeNode(3)),
+                    TreeNode(2,
+                             TreeNode(3), TreeNode(4)))
+    print(is_symmetric_tree(root))
 
     ## Test is_valid_parantheses
     print()
